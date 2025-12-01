ed144c0a132f9c0235e232143dfa5b94b76cb258
longwaan <1810193170@qq.com>
2025-12-01 10:49:13 +0800

    feat:顶部动态面包屑

--- src/layout/tabbar/breadcrumb/index.vue
+++ src/layout/tabbar/breadcrumb/index.vue
@@ -1,8 +1,9 @@
 <script setup lang="ts">
 import { ArrowRight } from '@element-plus/icons-vue';
 import useSettingFoldStore from '@/stores/modules/setting';
+import { useRoute } from 'vue-router';
 let useSettingStore = useSettingFoldStore()
-
+let $route =useRoute()
 const changeIcon=()=>{
   useSettingStore.fold=!useSettingStore.fold
 }
@@ -11,17 +12,19 @@ const changeIcon=()=>{
 
 <template>
   <div class="breadcrumb_container">
-    <div class="folder_container">
-      
+    <div class="folder_container">      
       <el-icon>
         <component :is="useSettingStore.fold?'Expand':'Fold'" @click="changeIcon"></component>
-      </el-icon>
-      
+      </el-icon>      
     </div>
 
     <el-breadcrumb :separator-icon="ArrowRight">
-      <el-breadcrumb-item :to="{ path: '/' }">homepage</el-breadcrumb-item>
-      <el-breadcrumb-item>promotion management</el-breadcrumb-item>
+      <el-breadcrumb-item v-for="(item,index) in $route.matched" :key="index" :to="item.path">
+      <el-icon>
+        <component :is="item.meta.icon"></component>
+      </el-icon>
+      <span style="margin-left: 5px;">{{ item.meta.title }}</span>
+      </el-breadcrumb-item>
     </el-breadcrumb>
   </div>
 </template>
