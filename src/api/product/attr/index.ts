a8eca5f7bc1bfe51b4acf6ec752e187fa296a8c7
longwaan <1810193170@qq.com>
2025-12-04 10:56:41 +0800

    feat(商品管理):属性管理添加/编辑属性

--- src/api/product/attr/index.ts
+++ src/api/product/attr/index.ts
@@ -1,22 +1,25 @@
 import request from "@/utils/request";
-import type { AttrResponseData, CategoryResponseData } from "./type";
+import type { AttrResponseData, CategoryResponseData,Attr } from "./type";
 
 enum API{
   GETC1_URL='/admin/product/getCategory1',
   GETC2_URL='/admin/product/getCategory2/',
   GETC3_URL='/admin/product/getCategory3/',
-  GETATTR_URL='/admin/product/attrInfoList/'
+  GETATTR_URL='/admin/product/attrInfoList/',
+  ADDORUPDATEATTR_URL='/admin/product/saveAttrInfo'
 }
 
 const reqC1Attr=()=>request.get<any,CategoryResponseData>(API.GETC1_URL)
 const reqC2Attr=(category1Id:number|string)=>request.get<any,CategoryResponseData>(API.GETC2_URL+category1Id)
 const reqC3Attr=(category2Id:number|string)=>request.get<any,CategoryResponseData>(API.GETC3_URL+category2Id)
 const reqAttr=(category1Id:number|string,category2Id:number|string,category3Id:number|string)=>request.get<any,AttrResponseData>(API.GETATTR_URL+`${category1Id}/${category2Id}/${category3Id}`)
+const reqAddOrUpdateAttr=(data:Attr)=>request.post(API.ADDORUPDATEATTR_URL,data)
 
 export {
   reqC1Attr,
   reqC2Attr,
   reqC3Attr,
   reqAttr,
+  reqAddOrUpdateAttr,
 
 }
--- src/views/product/attr/index.vue
+++ src/views/product/attr/index.vue
@@ -1,12 +1,19 @@
 <script setup lang="ts">
-import { reqAttr } from '@/api/product/attr';
-import type { AttrList } from '@/api/product/attr/type';
+import { reqAddOrUpdateAttr, reqAttr } from '@/api/product/attr';
+import type { AttrList, Attr, AttrResponseData } from '@/api/product/attr/type';
 import Category from '@/components/Category/index.vue'
-import { ref, watch } from 'vue';
+import { reactive, ref, watch } from 'vue';
 import useCategoryStore from '@/stores/modules/category';
+import { ElMessage } from 'element-plus';
 let categoryStore = useCategoryStore()
 let scene = ref<number>(0)
 let attrData = ref<AttrList>([])
+let attrParam = reactive<Attr>({
+  attrName: '',
+  categoryId: '',
+  categoryLevel: 3,
+  attrValueList: [],
+})
 
 watch(() => categoryStore.c3Id, () => {
   if (categoryStore.c3Id) {
@@ -16,11 +23,18 @@ watch(() => categoryStore.c3Id, () => {
 
 const getAttr = async () => {
   let result = await reqAttr(categoryStore.c1Id, categoryStore.c2Id, categoryStore.c3Id)
-  console.log(result)
+  // console.log(result)
   attrData.value = result.data
 }
 
 const addAttr = () => {
+  Object.assign(attrParam, {
+    attrName: '',
+    categoryId: categoryStore.c3Id,
+    categoryLevel: 3,
+    attrValueList: [],
+  })
+
   scene.value = 1;
 
 }
@@ -29,6 +43,37 @@ const cancel = () => {
   scene.value = 0;
 
 }
+
+const addAttrValue = () => {
+  attrParam.attrValueList.push({
+    valueName: '',
+  })
+}
+
+const editAttr = (row: Attr) => {
+  scene.value = 1
+  console.log(row)
+  Object.assign(attrParam, row)
+}
+
+const saveAttr = async () => {
+  let result: any = await reqAddOrUpdateAttr(attrParam)
+  if (result.code === 200) {
+    ElMessage({
+      type: 'success',
+      message: attrParam.id ? '编辑成功' : '添加成功'
+    })
+    scene.value = 0
+    getAttr()
+  } else {
+    ElMessage({
+      type: 'error',
+      message: attrParam.id ? '编辑失败' : '添加失败'
+    })
+    scene.value = 0
+    getAttr()
+  }
+}
 </script>
 
 <template>
@@ -36,39 +81,48 @@ const cancel = () => {
     <Category></Category>
     <el-card style="margin-top: 20px;">
       <div v-show="scene === 0">
-        <el-button type="primary" icon="Plus" @click="addAttr">添加平台属性</el-button>
+        <el-button type="primary" icon="Plus" @click="addAttr"
+          :disabled="categoryStore.c3Id ? false : true">添加平台属性</el-button>
         <el-table :data="attrData" style="width: 100%" border>
-          <el-table-column label="序号" width="100" type="index" align="center"/>
+          <el-table-column label="序号" width="100" type="index" align="center" />
           <el-table-column prop="attrName" label="属性名称" />
           <el-table-column label="属性名称值">
             <template #="{ row, index }">
-              <el-tag type="primary" v-for="(item, index) in row.attrValueList" style="margin-left: 5px;">{{ item.valueName }}</el-tag>
+              <el-tag type="primary" v-for="(item, index) in row.attrValueList" style="margin-left: 5px;">{{
+                item.valueName }}</el-tag>
             </template>
           </el-table-column>
           <el-table-column label="操作">
-            <el-button type="warning" icon="Edit"></el-button>
-            <el-button type="danger" icon="Delete"></el-button>
+            <template #="{ row, index }">
+              <el-button type="warning" icon="Edit" @click="editAttr(row)"></el-button>
+              <el-button type="danger" icon="Delete"></el-button>
+            </template>
           </el-table-column>
         </el-table>
       </div>
       <div v-show="scene === 1">
-        <el-form ref="formRef" :model="form">
+        <el-form>
           <el-form-item label="属性名称" prop="name">
-            <el-input v-model="input" placeholder="请输入" />
+            <el-input v-model="attrParam.attrName" placeholder="请输入" />
           </el-form-item>
           <el-form-item>
-            <el-button icon="Plus" type="primary">添加属性值</el-button>
+            <el-button icon="Plus" type="primary" :disabled="attrParam.attrName ? false : true"
+              @click="addAttrValue">添加属性值</el-button>
             <el-button @click="cancel">取消</el-button>
           </el-form-item>
-          <el-table :data="tableData" style="width: 100%">
-            <el-table-column prop="date" label="序号" width="100" />
-            <el-table-column prop="name" label="属性值名称" />
+          <el-table :data="attrParam.attrValueList" style="width: 100%">
+            <el-table-column label="序号" width="100" type="index" />
+            <el-table-column label="属性值名称">
+              <template #="{ row, index }">
+                <el-input v-model="row.valueName" placeholder="请输入" />
+              </template>
+            </el-table-column>
             <el-table-column prop="address" label="操作">
               <el-button icon="Delete"></el-button>
             </el-table-column>
           </el-table>
           <el-form-item>
-            <el-button type="primary">保存</el-button>
+            <el-button type="primary" @click="saveAttr">保存</el-button>
             <el-button @click="cancel">取消</el-button>
           </el-form-item>
         </el-form>
