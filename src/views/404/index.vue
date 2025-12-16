d7be05e46023fa2c95fc8f7410980a9526978da4
longwaan <1810193170@qq.com>
2025-12-16 15:38:39 +0800

    feat:404

--- src/views/404/index.vue
+++ src/views/404/index.vue
@@ -1,13 +1,24 @@
 <script setup lang="ts">
-
+import { useRouter } from 'vue-router'
+const router = useRouter()
+const goHome = () => {
+    router.push('/')
+}
 </script>
 
 <template>
-  <div>
-404
-  </div>
+    <div class="box">
+        <button @click="goHome">返回首页</button>
+        <img src="../../assets/images/error_images/404.png" alt="">
+    </div>
 </template>
 
 <style lang="scss" scoped>
-
+.box {
+    width: 100vw;
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
 </style>
